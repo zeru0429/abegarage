a2b22d9c9410b1658aec82cb907a066e5e18ddeb
zeru0429 <mihiretutigistu@gmail.com>
2024-02-17 22:21:51 +0300

    updating cors to make https://abegarage.netlify.app/ work

--- codebase/server/app.js
+++ codebase/server/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ 
-	origin: '*',
+	origin: ['https://abegarage.netlify.app/','*'],
 	credentials: true 
  }));
 app.use(appRoute);
